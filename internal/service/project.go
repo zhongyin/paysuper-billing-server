4c91450a7cb21a39ee56d37ca5ead7252cc84fd5
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-04-18 15:59:56 +0300

    PAY-0 project

---
+++ internal/service/project.go
@@ -0,0 +1,17 @@
+package service
+
+func (s *Service) ChangeProject() {
+
+}
+
+func (s *Service) GetProject() {
+
+}
+
+func (s *Service) ListProjects() {
+
+}
+
+func (s *Service) DeleteProject() {
+
+}
