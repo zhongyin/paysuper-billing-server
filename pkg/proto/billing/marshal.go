4a2e29286943eb4c5a7885886ee7e3b9de63ee31
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-04-22 22:44:43 +0300

    PAY-0 custom marshal function for refund

---
+++ pkg/proto/billing/marshal.go
@@ -0,0 +1,38 @@
+package billing
+
+import (
+	"encoding/json"
+	"github.com/golang/protobuf/ptypes/timestamp"
+)
+
+func (m *Refund) MarshalJSON() ([]byte, error) {
+	return json.Marshal(
+		&struct {
+			Id         string               `json:"id"`
+			OrderId    string               `json:"order_id"`
+			ExternalId string               `json:"external_id"`
+			Amount     float64              `json:"amount"`
+			CreatorId  string               `json:"creator_id"`
+			Reason     string               `json:"reason"`
+			Currency   string               `json:"currency"`
+			Status     int32                `json:"status"`
+			CreatedAt  *timestamp.Timestamp `json:"created_at"`
+			UpdatedAt  *timestamp.Timestamp `json:"updated_at"`
+			PayerData  *RefundPayerData     `json:"payer_data"`
+			SalesTax   float32              `json:"sales_tax"`
+		}{
+			Id:         m.Id,
+			OrderId:    m.Order.Uuid,
+			ExternalId: m.ExternalId,
+			Amount:     m.Amount,
+			CreatorId:  m.CreatorId,
+			Reason:     m.Reason,
+			Currency:   m.Currency.CodeA3,
+			Status:     m.Status,
+			CreatedAt:  m.CreatedAt,
+			UpdatedAt:  m.UpdatedAt,
+			PayerData:  m.PayerData,
+			SalesTax:   m.SalesTax,
+		},
+	)
+}
